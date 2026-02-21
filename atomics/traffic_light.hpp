4e3f451ceb274ec9522c31283844fb14afe6b370
gamze <gamzeatli2310@gmail.com>
2026-02-21 04:08:15 -0500

    create traffic_light atomic model

---
+++ atomics/traffic_light.hpp
@@ -0,0 +1,60 @@
+#ifndef TRAFFIC_LIGHT_HPP
+#define TRAFFIC_LIGHT_HPP
+
+#include <iostream>
+#include "cadmium/modeling/devs/atomic.hpp"
+
+using namespace cadmium;
+enum class LightColor {RED, YELLOW, GREEN};
+
+struct TrafficLightState {
+    LightColor color;
+
+    explicit TrafficLightState()
+    : color(GREEN), sigma(0.0) {}
+};
+
+#ifndef NO_LOGGING
+std::ostream& operator<<(std::ostream &out, const TrafficLightState& state) {
+    std::string colors[] = {"RED", "YELLOW", "GREEN"};
+    out << "{color: " << colors[(int)state.color] << "}";
+    return out;
+}
+#endif
+
+// Atomic Model: Traffic Light that gives color information to the road model.
+class TrafficLight : public Atomic<TrafficLightState> {
+public:
+    Port<LightColor> out_color;
+
+    TrafficLight(const std::string& id)
+        : Atomic<TrafficLightState>(id, TrafficLightState()) {
+        out_color = addOutPort<LightColor>("out_color");
+    }
+
+    void internalTransition(TrafficLightState& state) const override {
+        if (state.color == GREEN) {
+            state.color = RED;
+            state.sigma = 15.0; 
+        } else if (state.color == RED) {
+            state.color = YELLOW;
+            state.sigma = 5.0;
+        } else{
+            state.color = GREEN;
+            state.sigma = 10.0;
+        }
+
+    }
+
+	void externalTransition(TrafficLightState& state, double e) const override {}
+    
+    void output(const TrafficLightState& state) const override {
+        out_color->addMessage(LightColor(state.color));
+    }
+
+    [[nodiscard]] double timeAdvance(const TrafficLightState& state) const override {  
+        return state.sigma;
+    }
+};
+
+#endif 
